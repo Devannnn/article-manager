764b9551acc7e8c6e97b9eef7ae3638faf0410af
devan <devan.prgt@gmail.com>
2024-01-08 10:42:05 +0100

    Create DataTable

---
+++ frontend/src/components/Ajout.js
@@ -0,0 +1,14 @@
+// Bibliothèques
+import React from "react";
+
+function Ajout() {
+  return (
+    <div className="d-flex justify-content-end">
+      <button className="btn btn-success btn-lg">
+        Ajouter
+      </button>
+    </div>
+  );
+}
+
+export default Ajout;
---
+++ frontend/src/components/Tableau.js
@@ -0,0 +1,49 @@
+// Bibliothèques
+import React from "react";
+import { DataGrid } from '@mui/x-data-grid';
+import data from "../data/data.json";
+
+const columns = [
+    {
+        field: 'titre',
+        headerName: 'Titre',
+        width: 250,
+        renderCell: (params) => (
+            <a href={params.row.url_article} target="_blank" rel="noopener noreferrer">
+                {params.row.titre}
+            </a>
+        ),
+    },
+    { field: 'auteur', headerName: 'Auteur', width: 120 },
+    {
+        field: 'url_site',
+        headerName: 'Site',
+        width: 250,
+        renderCell: (params) => (
+            <a href={params.row.url_site} target="_blank" rel="noopener noreferrer">
+                {params.row.url_site}
+            </a>
+        ),
+    },
+    { field: 'date', headerName: 'Date', width: 100 },
+    {
+        field: 'synopsis',
+        headerName: 'Synopsis',
+        width: 400,
+        renderCell: (params) => (
+            <div style={{ whiteSpace: 'pre-line', wordWrap: 'break-word' }}>
+                {params.row.synopsis}
+            </div>
+        ),
+    },
+];
+
+function Tableau() {
+    return (
+        <div style={{ height: 300, width: '100%' }}>
+            <DataGrid rows={data} columns={columns} getRowHeight={() => 'auto'} />
+        </div>
+    )
+}
+
+export default Tableau;
